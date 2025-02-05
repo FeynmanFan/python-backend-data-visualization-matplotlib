6058c3fcfa75d7f5d1cb1a086b1b6d8f70b1fdac
FeynmanFan <cbehrens@gmail.com>
2025-02-05 15:30:50 -0600

    Added chart adornments

--- generic-line.py
+++ generic-line.py
@@ -15,10 +15,20 @@ y_values = data['data']['y']
 
 # plot it
 plt.figure()
-plt.plot(x_values, y_values, '-')
+plt.plot(x_values, y_values, '-', label='Request latency over time')
 
 plt.title(title)
 plt.xlabel(x_label)
 plt.ylabel(y_label)
+
+thresholds = [3, 4.5]
+threshold_labels = ['discount', 'free']
+
+plt.yticks(thresholds, threshold_labels)
+
+plt.grid(linestyle='--', alpha=.5)
+plt.subplots_adjust(bottom=.2) # 20% more at the bottom
+
+plt.legend(bbox_to_anchor=(.5, -.18), loc='upper left', borderaxespad=0.0)
 
 plt.show()
